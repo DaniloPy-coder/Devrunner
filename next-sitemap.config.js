c4b4570d520338a9042c0ee57db58f0979dc4778
Danilo <danilodevsp@gmail.com>
2025-09-28 17:23:12 -0300

    feat: add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://exporunner.com.br',
+    generateRobotsTxt: true,
+    changefreq: 'weekly',
+    priority: 0.7,
+};
+
+export default config;
