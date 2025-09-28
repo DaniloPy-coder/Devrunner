d64d7796b36e5e6fb322a8fee4bfa183a285cf14
Danilo <danilodevsp@gmail.com>
2025-09-28 17:38:59 -0300

    chore: chore link site

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-    siteUrl: 'https://exporunner.com.br',
+    siteUrl: 'https://www.exporunner.com.br',
     generateRobotsTxt: true,
     changefreq: 'weekly',
     priority: 0.7,
